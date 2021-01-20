83f1bce8649aa67f5dae1eeae6615093e9d9c029
Viraj <62030782+virajmane@users.noreply.github.com>
2021-01-21 03:00:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def get_tasks(abbr):
     return jsonify(user.val()["track"])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,use_reloader=True,threaded=True)
+    app.run(debug=True, host='0.0.0.0', port=5000,use_reloader=True,threaded=True)
